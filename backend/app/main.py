5ba71fdd61fcdecf900145e0c2c8863525064a57
a6hi <saraffabhishek@outlook.com>
2026-01-30 23:26:52 -0500

    added background task for scraper

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,29 @@
+import threading
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-
+from apscheduler.schedulers.background import BackgroundScheduler
+from app.services.scraping_service import scraper_wrapper
 from app.api.alerts import router as alerts_router
 from app.api.execute import router as execute_router
 
 app = FastAPI()
+scheduler = BackgroundScheduler()
+
+def run_scheduled_scraper():
+    scraper_wrapper()
+
+scheduler.add_job(scraper_wrapper, 'interval', minutes=5)
+
+@app.on_event("startup")
+async def startup_event():
+    scheduler.start() # Start the scheduler
+    threading.Thread(target=run_scheduled_scraper, daemon=True).start() 
+    print("FastAPI started and Scheduler is running..")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    scheduler.shutdown()
+    print("Scheduler shut down.")
 
 # GET /alerts (Feed the map)
 # GET /plan/{alert_id} (Fetches the AIs generated Response)

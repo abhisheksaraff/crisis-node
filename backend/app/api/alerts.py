87cc09ea7cacf4f78f60e19dc8cab9184281164e
a6hi <saraffabhishek@outlook.com>
2026-01-30 19:13:00 -0500

    setup mvc structure

---
+++ backend/app/api/alerts.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+from app.services.alerts_service import *
+
+router = APIRouter()
+
+@router.get("/")
+def read_alerts():
+    return get_all_alerts()
+
+@router.get("/{alert_id}")
+def read_alert(alert_id: int):
+    return get_alert(alert_id)
---
+++ backend/app/api/execute.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.services.execute_service import execute_alert
+
+router = APIRouter()
+
+@router.post("/{alert_id}")
+def post_execute(alert_id: int):
+    return execute_alert(alert_id)

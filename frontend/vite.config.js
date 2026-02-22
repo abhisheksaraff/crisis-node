eea6530108f24fac29156593772dedf38f964fca
a6hi <saraffabhishek@outlook.com>
2026-02-22 01:43:11 -0500

    setup GitHub Actions deployment for vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/crisis-node/",
   server: {
     host: true, // Listen on all local IPs
     port: 5173,
